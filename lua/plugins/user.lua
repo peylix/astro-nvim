c0d653f6b36b77e6e8b49378d8f8dd19ab121f13
Peylics <lisichen_2901@outlook.com>
2025-11-23 17:43:54 -0500

    Add glance.nvim

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -277,4 +277,14 @@ return {
     "nvim-treesitter/nvim-treesitter-context",
   },
 
+  {
+    "dnlhc/glance.nvim",
+    cmd = "Glance",
+    keys = {
+      { "gD", "<cmd>Glance definitions<cr>", desc = "Glance Definitions" },
+      { "gR", "<cmd>Glance references<cr>", desc = "Glance References" },
+      { "gY", "<cmd>Glance type_definitions<cr>", desc = "Glance Type Definitions" },
+      { "gM", "<cmd>Glance implementations<cr>", desc = "Glance Implementations" },
+    }
+  },
 }
