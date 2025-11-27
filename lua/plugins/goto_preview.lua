a75b8e328628e21ac567ec168f862040cd974197
Peylics <lisichen_2901@outlook.com>
2025-11-26 23:46:06 -0500

    Disable goto-preview in favor of glance.nvim

--- lua/plugins/goto_preview.lua
+++ lua/plugins/goto_preview.lua
@@ -1,5 +1,6 @@
 return {
   "rmagatti/goto-preview",
+  enabled = false,
   dependencies = { "rmagatti/logger.nvim" },
   event = "BufEnter",
   config = true, -- necessary as per https://github.com/rmagatti/goto-preview/issues/88
