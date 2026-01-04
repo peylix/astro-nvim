6cbd124e15e900775e5ad2519226b015d1366dcf
Peylics <lisichen_2901@outlook.com>
2026-01-04 20:54:35 +0800

    Enable neogit

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,5 +1,20 @@
 return {
   {
+    "NeogitOrg/neogit",
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed.
+      "folke/snacks.nvim", -- optional
+    },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gn", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+    },
+  },
+  {
     "topaxi/pipeline.nvim",
     keys = {
       { "<leader>ge", "<cmd>Pipeline<cr>", desc = "Open pipeline.nvim" },
