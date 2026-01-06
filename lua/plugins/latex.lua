599ea016522d5baf7ffa106ce22a26c04c6f73a2
Peylics <lisichen_2901@outlook.com>
2026-01-07 04:06:11 +0800

    Add Snacks integration to VimTex

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -1,10 +1,12 @@
 return {
   "lervag/vimtex",
-  lazy = false,     -- we don't want to lazy load VimTeX
+  lazy = false, -- we don't want to lazy load VimTeX
   ft = { "tex", "plaintex", "bib" },
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     -- VimTeX configuration goes here, e.g.
     vim.g.vimtex_view_method = "skim"
-  end
+    -- Snacks integration
+    vim.keymap.set("n", "<localleader>lt", function() return require("vimtex.snacks").toc() end)
+  end,
 }
