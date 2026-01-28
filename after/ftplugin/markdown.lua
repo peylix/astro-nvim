d3f222506ec26f0135b8a368d98dc40eaaa89b4e
Peylics <lisichen_2901@outlook.com>
2026-01-28 18:32:48 -0500

    Refine spelling check settings

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -5,21 +5,19 @@ vim.opt_local.showbreak = "↳ "
 
 -- set spell checking
 vim.opt_local.spell = true
-vim.opt_local.spelllang = "en_us"
-
-
+vim.opt_local.spelllang = { "en_us", "cjk" }
 
 -- Only for the current buffer
 local opts_expr = { expr = true, silent = true, buffer = true }
 local opts_local = { silent = true, buffer = true }
 
 -- if there is no count prefix, move by display lines, otherwise move by actual lines
-vim.keymap.set('n', 'j', function() return vim.v.count == 0 and 'gj' or 'j' end, opts_expr)
-vim.keymap.set('n', 'k', function() return vim.v.count == 0 and 'gk' or 'k' end, opts_expr)
+vim.keymap.set("n", "j", function() return vim.v.count == 0 and "gj" or "j" end, opts_expr)
+vim.keymap.set("n", "k", function() return vim.v.count == 0 and "gk" or "k" end, opts_expr)
 
 -- for visual mode
-vim.keymap.set({'x'}, 'j', 'gj', opts_local)
-vim.keymap.set({'x'}, 'k', 'gk', opts_local)
+vim.keymap.set({ "x" }, "j", "gj", opts_local)
+vim.keymap.set({ "x" }, "k", "gk", opts_local)
 
 -- move to beginning and end of display lines
 -- vim.keymap.set('n', '0', 'g0', opts_local)
--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -4,7 +4,7 @@ vim.opt_local.breakindent = true
 vim.opt_local.showbreak = "↳ "
 
 vim.opt_local.spell = true
-vim.opt_local.spelllang = "en_us"
+vim.opt_local.spelllang = { "en_us", "cjk" }
 
 -- vim.opt_local.conceallevel = 2
 -- vim.opt_local.concealcursor = "nc"
@@ -14,12 +14,12 @@ local opts_expr = { expr = true, silent = true, buffer = true }
 local opts_local = { silent = true, buffer = true }
 
 -- if there is no count prefix, move by display lines, otherwise move by actual lines
-vim.keymap.set('n', 'j', function() return vim.v.count == 0 and 'gj' or 'j' end, opts_expr)
-vim.keymap.set('n', 'k', function() return vim.v.count == 0 and 'gk' or 'k' end, opts_expr)
+vim.keymap.set("n", "j", function() return vim.v.count == 0 and "gj" or "j" end, opts_expr)
+vim.keymap.set("n", "k", function() return vim.v.count == 0 and "gk" or "k" end, opts_expr)
 
 -- for visual mode
-vim.keymap.set('x', 'j', 'gj', opts_local)
-vim.keymap.set('x', 'k', 'gk', opts_local)
+vim.keymap.set("x", "j", "gj", opts_local)
+vim.keymap.set("x", "k", "gk", opts_local)
 
 -- move to beginning and end of display lines
 -- vim.keymap.set('n', '0', 'g0', opts_local)
