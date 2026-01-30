a758866428d0d23e5563054be09c63adf7ac51f8
Peylics <lisichen_2901@outlook.com>
2026-01-29 20:44:24 -0500

    Add notification for the reduced mode

--- lua/profile.lua
+++ lua/profile.lua
@@ -3,4 +3,8 @@ local nvim_profile = vim.env.NVIM_PROFILE
 M.name = (nvim_profile == "reduced" or nvim_profile == "default") and nvim_profile or "default"
 M.is_default = M.name == "default"
 M.is_reduced = M.name == "reduced"
+-- notify if it is in reduced mode
+if M.is_reduced then
+    vim.notify("Neovim is running in reduced profile mode", vim.log.levels.WARN, { title = "Profile" })
+end
 return M
