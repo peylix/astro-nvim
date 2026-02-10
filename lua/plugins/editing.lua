d2c9fca14f63be4e9741ae25e090c4a91d7f2eab
Peylics <lisichen_2901@outlook.com>
2026-02-09 20:59:54 -0500

    Add harpoon and yanky.nvim

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -216,13 +216,55 @@ return {
   },
 
   {
-    "rmagatti/goto-preview",
-    enabled = false,
-    dependencies = { "rmagatti/logger.nvim" },
-    event = "BufEnter",
-    config = true, -- necessary as per https://github.com/rmagatti/goto-preview/issues/88
+    "gbprod/yanky.nvim",
+    dependencies = {
+      "folke/snacks.nvim",
+    },
     opts = {
-      default_mappings = true,
+      ring = {
+        history_length = 25,
+        storage = "shada",
+      },
+      highlight = {
+        -- avoid yanky.nvim overriding my custom yank highlight
+        on_put = false,
+        on_yank = false,
+      },
+    },
+    keys = {
+      {
+        "<leader>fy",
+        function() Snacks.picker.yanky() end,
+        mode = { "n", "x" },
+        desc = "Open Yank History",
+      },
+      { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
+      { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
+      { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
+      {
+        "gp",
+        "<Plug>(YankyGPutAfter)",
+        mode = { "n", "x" },
+        desc = "Put yanked text after cursor and leave cursor after",
+      },
+      {
+        "gP",
+        "<Plug>(YankyGPutBefore)",
+        mode = { "n", "x" },
+        desc = "Put yanked text before cursor and leave cursor after",
+      },
+      { "<c-p>", "<Plug>(YankyPreviousEntry)", desc = "Select previous entry through yank history" },
+      { "<c-n>", "<Plug>(YankyNextEntry)", desc = "Select next entry through yank history" },
+      { "]p", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put indented after cursor (linewise)" },
+      { "[p", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put indented before cursor (linewise)" },
+      { "]P", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put indented after cursor (linewise)" },
+      { "[P", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put indented before cursor (linewise)" },
+      { ">p", "<Plug>(YankyPutIndentAfterShiftRight)", desc = "Put and indent right" },
+      { "<p", "<Plug>(YankyPutIndentAfterShiftLeft)", desc = "Put and indent left" },
+      { ">P", "<Plug>(YankyPutIndentBeforeShiftRight)", desc = "Put before and indent right" },
+      { "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)", desc = "Put before and indent left" },
+      { "=p", "<Plug>(YankyPutAfterFilter)", desc = "Put after applying a filter" },
+      { "=P", "<Plug>(YankyPutBeforeFilter)", desc = "Put before applying a filter" },
     },
   },
 }
